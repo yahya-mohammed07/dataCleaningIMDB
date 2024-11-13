d3a5e332b04bb16d3322b6bee6bb4f24e104a50d
yahya-mohammed07 <yahyabaqer@proton.me>
2024-11-13 21:44:45 +0300

    almost done

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import pandas as pd
 import numpy as np
 import re
 
-data = pd.read_csv("messy_IMDB_dataset_cleaned.csv", sep=';',
-                   # Handle various null indicators
-                   quotechar='"', encoding='utf-8'
-                   )
+data: pd.DataFrame = pd.read_csv("messy_IMDB_dataset_cleaned.csv", sep=';',
+                                 # Handle various null indicators
+                                 quotechar='"', encoding='utf-8'
+                                 )
 
 data = data.replace('', np.nan)
 data = data.replace(' ', np.nan)
@@ -48,12 +48,15 @@ def cleaning(df: pd.DataFrame):
   df['Income'] = df['Income'].apply(
       lambda x: re.sub(r'(\d)(?=(\d{3})+(?!\d))', r'\1,', str(x)))
   df['Income'] = df['Income'].apply(
-      lambda x: re.sub(r'[a-zA-Z]', '', str(x)))
+      lambda x: re.sub(r'[a-zA-Z]', '', str(x)))  # remove all a-z
 
+  # votes
+  df['Votes'] = df['Votes'].astype(str).str.replace('.', ',', regex=False)
   return df
 
 
 result = cleaning(data)
 print(result.head())
 
-result.to_csv(r"D:\Programming\Master\dataCleaning\cleaned.csv", index=False)
+result.to_csv(
+    r"D:\Programming\Master\dataCleaning\cleaned.csv", index=False)
